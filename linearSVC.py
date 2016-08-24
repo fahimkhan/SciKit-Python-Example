fa8a8d25a2d28c1ec289be973b34fdbfdbd39cd0
fahimkhan <khanfahim2000@yahoo.com>
2016-08-24 16:34:35 +0530

    linear SVC

--- linearSVC.py
+++ linearSVC.py
@@ -12,6 +12,7 @@ style.use("ggplot")
 
 def Build_DataSet(features = ["DE Ratio","Trailing P/E"]):
     data_df = pd.DataFrame.from_csv("key_stats.csv")
+    data_df = data_df[:100]
 
     X = np.array(data_df[features].values)
     y = (data_df["Status"]
@@ -36,7 +37,7 @@ def Analysis():
 
     h0 = plt.plot(xx,yy,"k-",label="non weighted")
 
-    plt.scatter(X[:,0],X[:,1])
+    plt.scatter(X[:,0],X[:,1],c=y)
     
     plt.ylabel("Trailing P/E")
     plt.xlabel("DE Ratio")
